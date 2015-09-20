42125421be90a661be0d720d320c48c1df4b8178
tmarwen <marwen.trabelsi.insat@gmail.com>
2015-09-20 19:34:05 +0100

    Add place order feature as long as and admin app

---
+++ pro-angularjs/sports-store-chp-8/angularjs/components/auth/auth.js
@@ -0,0 +1,23 @@
+angular.module("sportsStoreAdmin")
+.constant("logoutUrl", "http://localhost:5500/users/logout")
+.constant("userInfoUrl", "http://localhost:5500/users/me")
+.directive("logout", function($http, $location, logoutUrl, userInfoUrl) {
+	return {
+		restrict: "E",
+		templateUrl: "components/auth/logout.html",
+		controller: function($scope) {
+
+			$scope.logout = function() {
+				$http.post(logoutUrl, { withCredentials: true })
+				.success(function(data) {
+					$scope.loggedUser = {};
+					$location.path("/login");
+				})
+				.error(function(error) {
+					$scope.error = error;
+				});
+			}
+
+		}
+	}
+});
